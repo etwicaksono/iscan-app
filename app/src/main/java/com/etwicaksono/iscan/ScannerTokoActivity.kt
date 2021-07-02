07ef3a0ae6d3266b25d21123643321d57c57a575
etwicaksono <echotechno@rocketmail.com>
2021-07-02 09:05:12 +0700

    tambah scanner

---
+++ app/src/main/java/com/etwicaksono/iscan/ScannerTokoActivity.kt
@@ -0,0 +1,11 @@
+package com.etwicaksono.iscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScannerTokoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
